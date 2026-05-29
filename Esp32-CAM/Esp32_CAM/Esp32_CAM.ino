50c53bcef953cde3d2a686b17a532caee0791fdb
Zmc18 <mzidaneakbari@gmail.com>
2026-05-30 00:24:48 +0700

    Esp32-CAM

--- Esp32-CAM/Esp32_CAM/Esp32_CAM.ino
+++ Esp32-CAM/Esp32_CAM/Esp32_CAM.ino
@@ -9,8 +9,8 @@
 #include <WiFi.h>
 
 // ─── KONFIGURASI WIFI ───────────────────────────────────
-#define WIFI_SSID "Absolute Solver"
-#define WIFI_PASS "CynIsMyRobo18z"
+#define WIFI_SSID "YOUR_WIFI_NAME"
+#define WIFI_PASS "YOUR_WIFI_PASSWORD"
 
 // ─── PIN KAMERA (AI-THINKER) ────────────────────────────
 #define PWDN_GPIO_NUM 32
